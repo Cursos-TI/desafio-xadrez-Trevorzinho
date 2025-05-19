81a74cb04c0679abd47955b4819f571d9a0c879c
Trevor <emanuelnunesdev@gmail.com>
2025-05-19 03:02:06 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-// nivel novato 
+// nivel aventureiro 
 
 int main() {
 
-    int i = 1;
+    int i = 1, passos = 1;
 
     // iniciando o valor de i como 1 para que ele possa mostrar quantas casas percorreu
     for (int i = 1; i <= 5; i++) {
@@ -24,11 +24,20 @@ int main() {
     while (i <= 5){
         printf("O bispo se moveu %d casa(s) para cima e para a direita.\n", i);
         i++;
+    };
+
+    while (passos--)
+    {
+        for (int i = 1; i < 3; i++) {
+            printf("O cavalo moveu %d casa(s) para baixo.\n", i);
+        }
+        printf("O cavalo moveu pra esquerda.\n", passos);
     }
 
 // Bispo: 5 casas na diagonal superior direita
 // Torre: 5 casas para a direita
 // Rainha: 8 casas para a esquerda
+//
 
     return 0;
 
